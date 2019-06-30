0248182e60b8d0633429ec032695750bbe109f1b
Mahmudul Hasan <simul.citycollege@gmail.com>
2019-06-30 13:32:46 +0100

    the image is loadong now

--- src/org/academiadecodigo/bootcamp/simplewebserver/WebServer.java
+++ src/org/academiadecodigo/bootcamp/simplewebserver/WebServer.java
@@ -43,12 +43,16 @@ public class WebServer {
             DataOutputStream out=new DataOutputStream(clientSocket.getOutputStream());
             BufferedReader in= new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String clientReq=requestHeader(in);
+            logger.log(Level.INFO,clientReq);
             File file=new File("www/index.html");
+            File file1= new File("www/logo.png");
             if(clientReq.equals("index.html")){
-                out.writeBytes("HTTP/1.1 200 Document Follows\r\n" +
-                        "Content-Type: text/html; charset=UTF-8\r\n"+
-                        "Content-Length:" + file.length()+"\r\n"+"\r\n");
                 FileInputStream fileIn=new FileInputStream(file);
+                out.writeBytes("HTTP/1.1 200 Document Follows\r\n");
+
+
+                out.writeBytes(  "Content-Type: text/html; charset=UTF-8\r\n"+
+                        "Content-Length:" + file.length()+"\r\n"+"\r\n");
                 byte[] buffer= new byte[1024];
                 int numbytes;
                 while((numbytes=fileIn.read(buffer))!=-1){
@@ -56,6 +60,20 @@ public class WebServer {
                 }
                 fileIn.close();
 
+            }
+            else if(clientReq.equals("logo.png")){
+
+                System.out.println("started writing the image");
+                FileInputStream fileIn=new FileInputStream(file1);
+                out.writeBytes("HTTP/1.1 200 Document Follows\r\n");
+                out.writeBytes(  "Content-Type: text/html; charset=UTF-8\r\n"+
+                        "Content-Length:" + file1.length()+"\r\n"+"\r\n");
+                byte[] buffer= new byte[1024];
+                int numbytes;
+                while((numbytes=fileIn.read(buffer))!=-1){
+                    out.write(buffer,0,numbytes);
+                }
+                fileIn.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
